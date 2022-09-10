0c6c11fca76719d7519582977d8524bc9d070631
BananaKun <yanxun951224@gmail.com>
2022-09-10 10:33:24 +0100

    update

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,8 +6,9 @@ const Footer = () => {
       <div className="section-title">Get In Touch</div>
       <div className="section-paragraph-container">
         <p>
-          Currently I&#39;m looking for new opportunities, If you have interest
-          on me please contact me through the email.
+          Currently I&#39;m not looking for new opportunities, If you have
+          interest on me please contact me through the email, I'm always up to
+          having a chat with new people.
         </p>
         <div className="btn-contact-container">
           <a href="mailto: yanxun951224@gmail.com" className="btn-contact">
